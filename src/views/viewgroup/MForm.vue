a979c829f7dc4487695ee7943541393e31efc54d
zhoushiming <marchmi@163.com>
2023-05-29 22:46:31 +0800

    page update：更新MForm组件示例

--- src/views/viewgroup/MForm.vue
+++ src/views/viewgroup/MForm.vue
@@ -2,10 +2,28 @@
   <app-main>
     <template #content>
       <div class="info">
-        <h3>基础表格：</h3>
+        <h3>基础表单：</h3>
         <div class="content">
           <m-form
             v-bind="basicForm"
+          >
+          </m-form>
+        </div>
+      </div>
+      <div class="info">
+        <h3>带操作表单：</h3>
+        <div class="content">
+          <m-form
+            v-bind="operationForm"
+          >
+          </m-form>
+        </div>
+      </div>
+      <div class="info">
+        <h3>表单校验：</h3>
+        <div class="content">
+          <m-form
+            v-bind="ruleForm"
           >
           </m-form>
         </div>
@@ -51,10 +69,10 @@
   import MForm from '@/components/MForm'
   import useForm from '@/components/hooks/useForm'
   import { enumToArray } from '@/utils'
-  import { infoMsg } from '@/utils/interaction'
+  import { infoMsg, successMsg } from '@/utils/interaction'
 
   /**
-   * 基础表格，只展示数据
+   * 基础表单，只展示数据
   */
   const basicForm = useForm({
     formFields: [
@@ -214,7 +232,7 @@
   /**
    * 含事件交互
   */
- const eventForm = useForm({
+  const eventForm = useForm({
     formFields: [
       {
         breakSign: '作品信息', // 表单中的上下文分割插槽
@@ -273,6 +291,202 @@
         label: '出版社',
         props: {
           placeholder: '请填写内容'
+        }
+      }
+    ]
+  })
+
+  /**
+   * 带操作表单
+  */
+  const operationForm = useForm({
+    formFields: [
+      {
+        breakSign: '作品信息', // 表单中的上下文分割插槽
+      },
+      {
+        key: 'bookName',
+        type: 'input',
+        label: '作品名称',
+        props: {
+          'show-word-limit': true,
+          maxlength: 20,
+          placeholder: '请输入作品名称'
+        }
+      },
+      {
+        key: 'author',
+        type: 'input',
+        label: '作者',
+        props: {
+          'show-word-limit': true,
+          maxlength: 10,
+          placeholder: '请输入作者'
+        }
+      },
+      {
+        key: 'tyee',
+        type: 'select',
+        label: '类型',
+        props: {
+          options: enumToArray({
+            1: '小说',
+            2: '漫画'
+          }),
+          placeholder: '请选择类型'
+        }
+      },
+      {
+        breakSign: '发行信息', // 表单中的上下文分割插槽
+      },
+      {
+        key: 'issuingDate',
+        type: 'datePicker',
+        label: '发行日期',
+        props: {
+          placeholder: '请选择日期'
+        }
+      },
+      {
+        key: 'press',
+        type: 'input',
+        label: '出版社',
+        props: {
+          placeholder: '请填写内容'
+        }
+      }
+    ],
+    operations: [
+      {
+        label: '保 存',
+        handler: (formData, ref) => {
+          successMsg(JSON.stringify(formData))
+        }, // 传递操作函数的指针
+        props: { // 其它的按钮控制属性
+          type: 'primary',
+          size: 'default'
+        }
+      },
+      {
+        label: '重 置',
+        handler: (formData, ref) => {
+          ref.resetFields()
+        }, // 传递操作函数的指针
+        props: { // 其它的按钮控制属性
+          type: 'info',
+          size: 'default'
+        }
+      }
+    ]
+  })
+
+  /**
+   * 自定义bookName校验规则
+  */
+  const bookNameValidator = (rule, value, callback) => {
+    if(!value){
+      callback()
+    }
+    if(value===ruleForm.dataFormParams.press){
+      callback(new Error('作品和出版社名称不能重复'))
+    }
+  }
+
+  /**
+   * 表单校验
+  */
+  const ruleForm = useForm({
+    formFields: [
+      {
+        breakSign: '作品信息', // 表单中的上下文分割插槽
+      },
+      {
+        key: 'bookName',
+        type: 'input',
+        label: '作品名称',
+        props: {
+          'show-word-limit': true,
+          maxlength: 20,
+          placeholder: '请输入作品名称'
+        }
+      },
+      {
+        key: 'author',
+        type: 'input',
+        label: '作者',
+        props: {
+          'show-word-limit': true,
+          maxlength: 10,
+          placeholder: '请输入作者'
+        }
+      },
+      {
+        key: 'tyee',
+        type: 'select',
+        label: '类型',
+        props: {
+          options: enumToArray({
+            1: '小说',
+            2: '漫画'
+          }),
+          placeholder: '请选择类型'
+        }
+      },
+      {
+        breakSign: '发行信息', // 表单中的上下文分割插槽
+      },
+      {
+        key: 'issuingDate',
+        type: 'datePicker',
+        label: '发行日期',
+        props: {
+          placeholder: '请选择日期'
+        }
+      },
+      {
+        key: 'press',
+        type: 'input',
+        label: '出版社',
+        props: {
+          placeholder: '请填写内容'
+        }
+      }
+    ],
+    rules: {
+      bookName: [
+        {
+          required: true, message: 'Please input book name', trigger: ['blur', 'change']
+        },
+        {
+          validator: bookNameValidator, trigger: ['blur', 'change']
+        }
+      ]
+    },
+    operations: [
+      {
+        label: '保 存',
+        handler: (formData, ref) => {
+          ref.validate((valid, fields) => {
+            if (valid) {
+              successMsg(JSON.stringify(formData))
+            } else {
+              console.log('error submit!', fields)
+            }
+          })
+        }, // 传递操作函数的指针
+        props: { // 其它的按钮控制属性
+          type: 'primary',
+          size: 'default'
+        }
+      },
+      {
+        label: '重 置',
+        handler: (formData, ref) => {
+          ref.resetFields()
+        }, // 传递操作函数的指针
+        props: { // 其它的按钮控制属性
+          type: 'info',
+          size: 'default'
         }
       }
     ]
