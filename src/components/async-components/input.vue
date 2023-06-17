ddb5d9843ea190db8942857418d13cee0b5773e0
zhoushiming <marchmi@163.com>
2023-06-17 10:30:51 +0800

    v-on绑定对象空值警告处理

--- src/components/async-components/input.vue
+++ src/components/async-components/input.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <mi-input v-model.trim="val" clearable v-bind="$attrs"  v-on="$attrs.events"/>
+    <mi-input v-model.trim="val" clearable v-bind="$attrs" v-on="$attrs.events || {}"/>
   </div>
 </template>
 <script>
