20be8afa399fe26cfe688420fbe33ea349886efb
zhoushiming <marchmi@163.com>
2023-06-19 22:14:41 +0800

    添加uuid生成和列表分页逻辑

--- src/plugins/useForage.js
+++ src/plugins/useForage.js
@@ -1,4 +1,30 @@
 import localforage, { key } from 'localforage'
+import { clearEmpty } from '@/utils'
+
+function generateUUID() {
+  var d = new Date().getTime();
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+    d += performance.now() // use high-precision timer if available
+  }
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random()*16)%16 | 0
+    d = Math.floor(d/16)
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16)
+  })
+  return uuid
+}
+
+function checkAllConditions (filter, value) {
+  const keys = Object.keys(filter)
+  let flag = true
+  for(let i = 0; i < keys.length; i++){
+    if(!value[keys[i]] || !value[keys[i]].includes(filter[keys[i]])){
+      flag = false
+      break
+    }
+  }
+  return flag
+}
 
 const useForage = (dbName, storeName) => {
 
@@ -22,12 +48,13 @@ const useForage = (dbName, storeName) => {
 
   /**
    * 新增或更新一条数据
-   * @param {*} key 
    * @param {*} value 
    * @returns promise
    */
-  const setItem = (key, value) => {
-    return forage.setItem(key, value)
+  const setItem = (value) => {
+    let uuid = value.uuid || generateUUID()
+    value.uuid = uuid
+    return forage.setItem(uuid, JSON.parse(JSON.stringify(value)))
   }
 
   /**
@@ -61,14 +88,24 @@ const useForage = (dbName, storeName) => {
     const total = await getLength()
     const result = { total }
     const { pageNum, pageSize } = query
-    const filter = { ...query }
+    const filter = clearEmpty({ ...query })
     delete filter.pageNum
     delete filter.pageSize
+    delete filter.total
+    console.log(filter, pageNum, pageSize)
+    const conditions = Object.keys(filter)
     const gather = []
     await forage.iterate((value, key, idx)=> {
-      gather.push(value)
+      if(!conditions.length){
+        gather.push(value)
+      }else if(checkAllConditions(filter,value)){
+        gather.push(value)
+      }
     })
-    result.list = gather
+    result.total = gather.length
+    const startIdx = (pageNum - 1) * pageSize
+    const endIdx = pageNum * pageSize >  result.total ?  result.total : pageNum * pageSize
+    result.list = gather.splice(startIdx,endIdx)
     return result
   }
 
